451855f5287caa886a85e410d2cdbab803a6224a
yamunaKadava <163979436+yamunaKadava@users.noreply.github.com>
2024-03-20 12:57:41 +0530

    [Rohit|Yamuna] Add. Welcome Screen asking Name (#2)
    * [Rohit|Yamuna] Add. Welcome Screen asking Name
    
    * [Rohit|Yamuna] Add. Navigator
    
    * [Rohit|Yamuna] Fix. Removal of talismanrc

---
+++ src/features/welcome/InputName.tsx
@@ -0,0 +1,39 @@
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useState } from 'react'
+import Welcome from './Welcome';
+
+
+export default function InputName() {
+    const [name,setName]=useState("");
+    const handleClick=()=>{
+        if(name===""){
+            console.log("Empty");
+        }
+        else{
+            console.log(name);
+        }
+    }
+
+  return (
+    <View style={{justifyContent:'center',height:'100%',padding:10}}>
+      <TextInput style={{
+        height:50,
+        borderWidth:2,
+        borderColor:'grey',
+      }}
+      onChangeText={(v)=>{setName(v)}}
+      placeholder='Enter your name..'
+      />
+      <View  style={styles.buttonStyles}>
+      <Button title='submit' />
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    buttonStyles:{
+        alignItems:'center',
+        marginTop:4,
+    }
+})
