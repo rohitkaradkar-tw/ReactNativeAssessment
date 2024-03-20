75beceba0ab29c6391f32ee10d2a103b1b8b3af3
Rohit Karadkar <rohit.karadkar@thoughtworks.com>
2024-03-20 15:37:24 +0530

    Store username to shared preferences

--- src/features/welcome/InputName.tsx
+++ src/features/welcome/InputName.tsx
@@ -1,39 +1,62 @@
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Welcome from './Welcome';
+import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 export default function InputName() {
-    const [name,setName]=useState("");
-    const handleClick=()=>{
-        if(name===""){
-            console.log("Empty");
-        }
-        else{
-            console.log(name);
-        }
+  const navigation = useNavigation();
+  const [name, setName] = useState("");
+  const storeUserName = async (value: string) => {
+    try {
+      await AsyncStorage.setItem('name', name);
+    } catch (e) {
+      console.log("Error storing username: " + e);
     }
+  }
+  const handleClick = () => {
+    if (name === "") {
+      console.log("Empty");
+    }
+    else {
+      console.log(name);
+      storeUserName(name);
+      navigation.navigate('Welcome');
+    }
+  }
+
+  const getName = async () => AsyncStorage.getItem('name');
+
+  useEffect(() => {
+    getName().then((value) => {
+      if (value !== null) {
+        console.log('Name already exists: ' + value + ' navigating to Welcome'); 
+        navigation.navigate('Welcome');
+      }
+    });
+  });
 
   return (
-    <View style={{justifyContent:'center',height:'100%',padding:10}}>
+    <View style={{ justifyContent: 'center', height: '100%', padding: 10 }}>
       <TextInput style={{
-        height:50,
-        borderWidth:2,
-        borderColor:'grey',
+        height: 50,
+        borderWidth: 2,
+        borderColor: 'grey',
       }}
-      onChangeText={(v)=>{setName(v)}}
-      placeholder='Enter your name..'
+        onChangeText={(v) => { setName(v) }}
+        placeholder='Enter your name..'
       />
-      <View  style={styles.buttonStyles}>
-      <Button title='submit' />
+      <View style={styles.buttonStyles}>
+        <Button title='submit' onPress={handleClick} />
       </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    buttonStyles:{
-        alignItems:'center',
-        marginTop:4,
-    }
+  buttonStyles: {
+    alignItems: 'center',
+    marginTop: 4,
+  }
 })
--- src/features/welcome/Welcome.tsx
+++ src/features/welcome/Welcome.tsx
@@ -1,13 +1,26 @@
 // The app asks for my name when I open it for first time, remembers it and shows “Hey <my_name>” when I close and open it afterwards
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Welcome() {
-  return (
-    <View>
-      <Text>Welcome</Text>
-    </View>
-  )
+    const [name, setName] = React.useState("");
+
+    async function getName() {
+        return await AsyncStorage.getItem('name');
+    }
+
+    useEffect(() => {
+        getName().then((value) => {
+            if (value !== null) setName(value);
+        });
+    })
+
+    return (
+        <View>
+            <Text>Welcome {name}</Text>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({})
