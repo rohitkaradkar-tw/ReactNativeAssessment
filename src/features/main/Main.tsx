130ecccd6e5e4f058a16b115c13469b795e09639
Rohit Karadkar <rohit.karadkar@thoughtworks.com>
2024-03-22 11:33:08 +0530

    Add  dark mode
    Dark mode Welcome screen
    
    Dark mode on welcome screen

--- src/features/main/Main.tsx
+++ src/features/main/Main.tsx
@@ -1,29 +1,31 @@
 // This app will have 4 bottom tabs (Home, Wishlist, Cart, Settings). And will show me the title of the page whenever I navigate to it. 
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { DarkTheme, DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home from './home/Home';
-import Settings from './settings/Settings';
-import WishList from './wishlist/WishList';
-import Cart from './cart/Cart';
-
+import Home from './Home';
+import Settings from './Settings';
+import WishList from './WishList';
+import Cart from './Cart';
+import { ThemeModeContext } from '../../../App';
 
 const Tab = createBottomTabNavigator();
 
 const MainScreen = () => {
+    const themeModeContext = React.useContext(ThemeModeContext);
+    const themeData = themeModeContext.themeMode === 'dark' ? DarkTheme : DefaultTheme;
     return (
-        <NavigationContainer independent={true}>
+        <NavigationContainer independent={true} theme={themeData}>
             <Tab.Navigator
-            initialRouteName="Home" 
-            screenOptions={{
-                tabBarActiveTintColor:'purple',
-                tabBarShowLabel:true,
-            }}
+                initialRouteName="Home"
+                screenOptions={{
+                    tabBarActiveTintColor: 'purple',
+                    tabBarShowLabel: true,
+                }}
             >
-                <Tab.Screen name="Home" component={Home} options={{title:'Home'}} />
-                <Tab.Screen name="Wishlist" component={WishList} options={{title:'Wishlist'}}/>
-                <Tab.Screen name='Cart' component={Cart} options={{title:'Cart'}}/>
-                <Tab.Screen name="Settings" component={Settings} options={{title:'Settings'}} />
+                <Tab.Screen name="Home" component={Home} options={{ title: 'Home' }} />
+                <Tab.Screen name="Wishlist" component={WishList} options={{ title: 'Wishlist' }} />
+                <Tab.Screen name='Cart' component={Cart} options={{ title: 'Cart' }} />
+                <Tab.Screen name="Settings" component={Settings} options={{ title: 'Settings' }} />
             </Tab.Navigator>
         </NavigationContainer>
     );
