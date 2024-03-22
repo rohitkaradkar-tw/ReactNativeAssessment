60577295ad8ba71591f4efa4fdb72f705d0df5a2
Rohit Karadkar <140815658+rohitkaradkar-tw@users.noreply.github.com>
2024-03-22 14:29:38 +0530

    Add  dark mode (#8)
    Dark mode Welcome screen
    
    Dark mode on welcome screen

--- App.tsx
+++ App.tsx
@@ -1,26 +1,38 @@
-import React, { useEffect } from 'react'
+import React, { createContext, useEffect } from 'react'
 import InputNameScreen from './src/features/welcome/InputNameScreen'
-import { NavigationContainer } from '@react-navigation/native'
+import { DarkTheme, DefaultTheme, NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import WelcomeScreen from './src/features/welcome/WelcomeScreen';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import LoadingScreen from './src/component/LoadingScreen';
 import Main from './src/features/main/Main';
 
+
 type RootStackParamList = {
-  InitialInputName:undefined,
+  InitialInputName: undefined,
   InputName: undefined,
   Welcome: { username: string },
   Loading: undefined,
-  Main:undefined,
+  Main: undefined,
 }
 
-const RootStack = createNativeStackNavigator<RootStackParamList>();
+export type ThemeMode = 'light' | 'dark' | 'auto';
+
+interface ThemeModeContextProps {
+  themeMode: ThemeMode;
+  setThemeMode: React.Dispatch<React.SetStateAction<ThemeMode>>;
+}
+
+export const ThemeModeContext = createContext<ThemeModeContextProps>({
+  themeMode: 'light',
+  setThemeMode: () => { },
+});
+
+export const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
-  // a state which has isLoading and userName properties
-
   const [state, setState] = React.useState({ isLoading: true, userName: '' })
+  const [themeMode, setThemeMode] = React.useState<ThemeMode>('dark');
 
   useEffect(() => {
     const getUsername = async () => await AsyncStorage.getItem('username')
@@ -30,26 +42,34 @@ export default function App() {
     })
   })
 
+  const themeData = themeMode === 'dark' ? DarkTheme : DefaultTheme
+
   if (state.isLoading) {
-    return <LoadingScreen />
+    return (
+      <ThemeModeContext.Provider value={{ themeMode: themeMode, setThemeMode: setThemeMode }}>
+        <LoadingScreen />
+      </ThemeModeContext.Provider>
+    )
   }
   return (
-    <NavigationContainer>
-      <RootStack.Navigator>
-        <>
-          {
-            state.userName == ''
-              ? <>
-                <RootStack.Screen name='InitialInputName' component={InputNameScreen} />
-                <RootStack.Screen name='Welcome' component={WelcomeScreen} />
-              </>
-              : <RootStack.Screen name='Welcome' component={WelcomeScreen} initialParams={{ username: state.userName }} options={{title:'Welcome to Amazon'}}/>
-          }
-          <RootStack.Screen name='Main' component={Main} options={{title:'Amazon'}} />
-          <RootStack.Screen name='InputName' component={InputNameScreen} />
-        </>
-      </RootStack.Navigator>
-    </NavigationContainer>
+    <ThemeModeContext.Provider value={{ themeMode: themeMode, setThemeMode: setThemeMode }}>
+      <NavigationContainer theme={themeData}>
+        <RootStack.Navigator>
+          <>
+            {
+              state.userName == ''
+                ? <>
+                  <RootStack.Screen name='InitialInputName' component={InputNameScreen} />
+                  <RootStack.Screen name='Welcome' component={WelcomeScreen} />
+                </>
+                : <RootStack.Screen name='Welcome' component={WelcomeScreen} initialParams={{ username: state.userName }} options={{ title: 'Welcome to Amazon' }} />
+            }
+            <RootStack.Screen name='Main' component={Main} options={{ title: 'Amazon' }} />
+            <RootStack.Screen name='InputName' component={InputNameScreen} />
+          </>
+        </RootStack.Navigator>
+      </NavigationContainer>
+    </ThemeModeContext.Provider>
   )
 }
 export type { RootStackParamList }
