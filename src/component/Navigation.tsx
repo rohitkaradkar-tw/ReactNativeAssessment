596cfe079c08bc0442c6555729708b3b5404095b
yamunaKadava <163979436+yamunaKadava@users.noreply.github.com>
2024-03-25 17:54:38 +0530

    Yk/25 03 2024/navigation (#13)
    * [Yamuna] Refactor. created  new component for navigations

---
+++ src/component/Navigation.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import WelcomeScreen from '../features/welcome/WelcomeScreen';
+import InputNameScreen from '../features/welcome/InputNameScreen';
+import Main from '../features/main/Main';
+
+export type RootStackParamList = {
+  InitialInputName: undefined;
+  InputName: undefined;
+  Welcome: { username: string };
+  Loading: undefined;
+  Main: undefined;
+};
+
+export const RootStack = createNativeStackNavigator<RootStackParamList>();
+
+const Navigation = (props: any) => {
+  return (
+    <NavigationContainer theme={props.theme}>
+      <RootStack.Navigator>
+        <>
+          {props.state.userName === '' ? (
+            <>
+              <RootStack.Screen
+                name="InitialInputName"
+                component={InputNameScreen}
+                options={{ title: 'Login' }}
+              />
+              <RootStack.Screen name="Welcome" component={WelcomeScreen} />
+            </>
+          ) : (
+            <RootStack.Screen
+              name="Welcome"
+              component={WelcomeScreen}
+              initialParams={{ username: props.state.userName }}
+              options={{ title: 'Welcome to Amazon' }}
+            />
+          )}
+          <RootStack.Screen
+            name="Main"
+            component={Main}
+            options={{ title: 'Amazon' }}
+          />
+          <RootStack.Screen
+            name="InputName"
+            component={InputNameScreen}
+            options={{ title: 'Edit Name' }}
+          />
+        </>
+      </RootStack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
