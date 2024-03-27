8e3c7c56f56c803652ee4c88adb18808bf5b3642
Suresh Kumar Putchala <psureshk9@gmail.com>
2024-03-27 13:36:01 +0530

    Added wishlist screen and feature

---
+++ src/features/wishlist/WishListIcon.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Image, Pressable, StyleSheet, View } from 'react-native';
+import { useStoreData } from '../../datastore/DataStoreProvider';
+
+const selectedHeartIcon = '../../../assets/images/heartSelected.png';
+const unSelectedHeartIcon = '../../../assets/images/wishlist.png';
+
+export const WishListIcon = ({ productID }: { productID: number }) => {
+  const { isWishlisted, toggleWishStatus } = useStoreData();
+
+  return (
+    <Pressable
+      onPress={() => {
+        toggleWishStatus(productID);
+      }}
+      style={styles.iconButton}>
+      {isWishlisted(productID) ? (
+        <Image
+          source={require(selectedHeartIcon)}
+          style={styles.icon}
+          resizeMode="contain"
+        />
+      ) : (
+        <View style={styles.iconBox}>
+          <Image
+            source={require(unSelectedHeartIcon)}
+            style={styles.icon}
+            resizeMode="contain"
+          />
+        </View>
+      )}
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  icon: {
+    aspectRatio: 1,
+    height: '100%'
+  },
+  iconButton: {
+    position: 'absolute',
+    top: 15,
+    right: 15,
+    zIndex: 10,
+    aspectRatio: 1,
+    width: 30
+  },
+  iconBox: {
+    padding: 4,
+    borderRadius: 100,
+    opacity: 0.5
+  }
+});
