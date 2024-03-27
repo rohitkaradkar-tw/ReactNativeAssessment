b2b46f04e830a4ed8bff471f0a2d2a13fb6a142d
Suresh Kumar Putchala <psureshk9@gmail.com>
2024-03-27 11:26:43 +0530

    change some styles

--- src/navigation/ScreenNavigation.tsx
+++ src/navigation/ScreenNavigation.tsx
@@ -6,14 +6,36 @@ import Cart from '../features/screens/Cart';
 import { Home } from '../features/screens/Home';
 import WishList from '../features/screens/WishList';
 import Settings from '../features/screens/Settings';
+import {
+  CartTabIcon,
+  HomeTabIcon,
+  SettingsTabIcon,
+  WishlistTabIcon
+} from '../features/components/TabBarIcon';
 
 const Tab = createBottomTabNavigator();
 
 export const ScreenNavigator = () => (
   <Tab.Navigator initialRouteName={TabScreens.HOME}>
-    <Tab.Screen name={TabScreens.HOME} component={Home} />
-    <Tab.Screen name={TabScreens.WISHLIST} component={WishList} />
-    <Tab.Screen name={TabScreens.CART} component={Cart} />
-    <Tab.Screen name={TabScreens.SETTING} component={Settings} />
+    <Tab.Screen
+      name={TabScreens.HOME}
+      component={Home}
+      options={{ tabBarIcon: HomeTabIcon }}
+    />
+    <Tab.Screen
+      name={TabScreens.WISHLIST}
+      component={WishList}
+      options={{ tabBarIcon: WishlistTabIcon }}
+    />
+    <Tab.Screen
+      name={TabScreens.CART}
+      component={Cart}
+      options={{ tabBarIcon: CartTabIcon }}
+    />
+    <Tab.Screen
+      name={TabScreens.SETTING}
+      component={Settings}
+      options={{ tabBarIcon: SettingsTabIcon }}
+    />
   </Tab.Navigator>
 );
