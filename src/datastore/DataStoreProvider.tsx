096e23ab8bd024817ba007b1fdddf6ea597c68dc
Yamuna Kadava <yamuna.kadava@thoughtworks.com>
2024-03-27 14:47:17 +0530

    [Yamuna] Add. context for Cart

--- src/datastore/DataStoreProvider.tsx
+++ src/datastore/DataStoreProvider.tsx
@@ -7,6 +7,8 @@ interface DataStoreContextProps {
   toggleWishStatus: (selectedId: number) => void;
   getWishList: () => ProductType[];
   isWishlisted: (selectedId: number) => boolean;
+  toggleCartItems: (selectedId: number) => void;
+  getCartList: () => ProductType[];
 }
 
 const noOp = () => {};
@@ -15,7 +17,9 @@ const DataStoreContext = createContext<DataStoreContextProps>({
   data: [],
   toggleWishStatus: noOp,
   getWishList: () => [],
-  isWishlisted: () => false
+  isWishlisted: () => false,
+  toggleCartItems: noOp,
+  getCartList: () => []
 });
 
 export const useStoreData = () => {
@@ -30,6 +34,7 @@ export const useStoreData = () => {
 export const DataStoreProvider = ({ children }: any) => {
   const [data, setData] = useState<ProductType[]>([]);
   const [wishlistIDs, setWishListIDs] = useState<Set<number>>(new Set());
+  const [cartListIDs, setCartListIDs] = useState<Set<number>>(new Set());
 
   // Wishlist related operations
   const getWishList = () => data.filter(product => wishlistIDs.has(product.id));
@@ -47,13 +52,35 @@ export const DataStoreProvider = ({ children }: any) => {
     setWishListIDs(wishlist);
   };
 
+  //Cart Items related operations
+  const getCartList = () => data.filter(product => cartListIDs.has(product.id));
+  const toggleCartItems = (selectedID: number) => {
+    const cart = new Set<number>([...cartListIDs]);
+
+    if (cart.has(selectedID)) {
+      cart.delete(selectedID);
+      setCartListIDs(cart);
+      return;
+    }
+
+    cart.add(selectedID);
+    setCartListIDs(cart);
+  };
+
   useEffect(() => {
     fetchProducts().then(responseJson => setData(responseJson));
   }, []);
 
   return (
     <DataStoreContext.Provider
-      value={{ data, toggleWishStatus, getWishList, isWishlisted }}>
+      value={{
+        data,
+        toggleWishStatus,
+        getWishList,
+        isWishlisted,
+        toggleCartItems,
+        getCartList
+      }}>
       {children}
     </DataStoreContext.Provider>
   );
