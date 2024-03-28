21e3d0ba920993c42ae6781ce5bc228ca39050db
VikramKadam <vikram.kadam@thoughtworks.com>
2024-03-28 13:46:38 +0530

    Get saved name from storage and navigate to main screen

--- src/features/welcome/InputNameScreen.tsx
+++ src/features/welcome/InputNameScreen.tsx
@@ -1,24 +1,43 @@
 import { useTheme } from '@react-navigation/native';
-import React, { useState } from 'react';
-import { StyleSheet, TextInput, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 import { StackScreens } from '../../navigation/Screens';
+import { getUserName, setUserName } from '../../service/store';
 import { Screen } from '../components/Screen';
 
 export const InputNameScreen = ({ navigation }: any) => {
-  const [name, setName] = useState('');
   const { colors } = useTheme();
-  // TODO : if already name exist navigate to content
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const storedName = await getUserName();
+        console.log('User Name: ' + storedName);
+        if (storedName) {
+          navigation.replace(StackScreens.CONTENT);
+        }
+      } catch (error) {
+        console.error('Error retrieving user data:', error);
+      }
+    };
+
+    getUserData();
+  }, [navigation]);
 
   const handleSubmit = () => {
     if (name.length >= 3) {
-      // TODO : save name it somewhere
+      setUserName(name);
       navigation.navigate(StackScreens.CONTENT);
     }
   };
 
   return (
     <Screen>
+      <View style={styles.titleBox}>
+        <Text style={[styles.title, { color: colors.text }]}>Welcome</Text>
+      </View>
       <View style={styles.container}>
         <TextInput
           style={[styles.textInput, { color: colors.text }]}
@@ -50,5 +69,25 @@ const styles = StyleSheet.create({
     borderRadius: 100,
     borderWidth: 1,
     aspectRatio: 1
+  },
+  screen: {
+    justifyContent: 'center',
+    height: '100%',
+    padding: 20
+  },
+  buttonStyles: {
+    alignItems: 'center',
+    height: 40,
+    backgroundColor: '#5297eb',
+    marginTop: 30
+  },
+  titleBox: {
+    height: 100,
+    alignContent: 'center'
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    textAlign: 'center'
   }
 });
