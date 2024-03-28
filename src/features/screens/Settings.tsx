c5cf23e5acec2cdce798498a440096ef69cb733d
VikramKadam <vikram.kadam@thoughtworks.com>
2024-03-28 16:53:41 +0530

    Udate user name

--- src/features/screens/Settings.tsx
+++ src/features/screens/Settings.tsx
@@ -1,24 +1,80 @@
-import React, { useState } from 'react';
-import { StyleSheet, Switch, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  Switch,
+  Text,
+  View,
+  TextInput,
+  Pressable,
+  Alert
+} from 'react-native';
+import { Button } from 'react-native-paper';
 import { useTheme } from '@react-navigation/native';
 
 import { useThemeContext } from '../../theme/AppTheme';
+import { getUserName, setUserName } from '../../service/store';
+import { Divider } from 'react-native-paper';
 
 export default function Settings() {
   const [switchState, setSwitchState] = useState(false);
   const { toggleTheme } = useThemeContext();
   const { colors } = useTheme();
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const storedName = await getUserName();
+        console.log('User Name: ' + storedName);
+        if (storedName) {
+          setName(storedName);
+        }
+      } catch (error) {
+        console.error('Error retrieving user data:', error);
+      }
+    };
+
+    getUserData();
+  }, []);
 
   const toggleSwitch = () => {
     toggleTheme();
     setSwitchState(!switchState);
   };
 
+  const saveUserName = () => {
+    console.log('Save user name: ' + name);
+    setUserName(name);
+    Alert.alert('App', 'Name updated successfully', [
+      {
+        text: 'Close',
+        style: 'cancel'
+      }
+    ]);
+  };
+
   return (
     <View style={[styles.screen, { backgroundColor: colors.background }]}>
-      <View style={styles.switch}>
-        <Text style={{ color: colors.text }}>Dark Theme</Text>
-        <Switch onChange={toggleSwitch} value={switchState} />
+      <View style={[styles.card, { backgroundColor: colors.card }]}>
+        <View style={styles.switch}>
+          <Text style={{ color: colors.text }}>Dark Theme</Text>
+          <Switch onChange={toggleSwitch} value={switchState} />
+        </View>
+        <Divider />
+        <View style={styles.switch}>
+          <TextInput
+            value={name}
+            style={[styles.textInput, { color: colors.text }]}
+            onChangeText={setName}
+          />
+          <Pressable
+            onPress={() => {
+              console.log('on button press');
+              saveUserName();
+            }}>
+            <Button style={styles.buttonStyles}>Edit</Button>
+          </Pressable>
+        </View>
       </View>
     </View>
   );
@@ -26,14 +82,39 @@ export default function Settings() {
 
 const styles = StyleSheet.create({
   screen: {
+    width: '100%',
     height: '100%',
-    justifyContent: 'center',
-    padding: 24
+    padding: 15,
+    paddingTop: 20
+  },
+  card: {
+    padding: 20,
+    width: '100%',
+    borderRadius: 20,
+    gap: 10,
+    position: 'relative'
   },
   switch: {
+    width: '100%',
     flexDirection: 'row',
     gap: 10,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'space-between'
+  },
+  textInput: {
+    height: 40,
+    width: '70%',
+    borderWidth: 0.5,
+    borderColor: 'grey',
+    marginTop: 10,
+    paddingLeft: 15,
+    paddingRight: 15
+  },
+  buttonStyles: {
+    alignItems: 'center',
+    marginTop: 10,
+    height: 40,
+    width: 70,
+    backgroundColor: '#5297eb'
   }
 });
